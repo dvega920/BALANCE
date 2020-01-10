fabbe60ffdd57c3ef63f05ced9d4cc50fea643a2
David Vega <dvega920@gmail.com>
2020-01-09 20:42:59 -0500

    added schema/seed.sql files

--- database/schema.sql
+++ database/schema.sql
@@ -1,2 +1,27 @@
-DROP DATABASE IF EXISTS `sample_db`;
-CREATE DATABASE `sample_db`;
+DROP DATABASE IF EXISTS balance_db;
+CREATE DATABASE balance_db;
+
+use balance_db;
+
+CREATE TABLE questions
+(
+    id INTEGER
+    AUTO_INCREMENT NOT NULL,
+    question VARCHAR
+    (255) NOT NULL,
+    user_res INTEGER,
+    createdAt DATETIME,
+    PRIMARY KEY
+    (id)
+);
+
+    CREATE TABLE activities
+    (
+        id INTEGER
+        AUTO_INCREMENT NOT NULL,
+    name VARCHAR
+        (255) NOT NULL,
+    createdAt DATETIME,
+    PRIMARY KEY
+        (id) 
+)
