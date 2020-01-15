1016bc0d9345e041599dc66881b35384b42809d4
David Vega <dvega920@gmail.com>
2020-01-15 12:52:13 -0500

    Removed questions table query

--- database/schema.sql
+++ database/schema.sql
@@ -3,25 +3,4 @@ CREATE DATABASE balance_db;
 
 use balance_db;
 
-CREATE TABLE questions
-(
-    id INTEGER
-    AUTO_INCREMENT NOT NULL,
-    question VARCHAR
-    (255) NOT NULL,
-    user_res INTEGER,
-    createdAt DATETIME,
-    PRIMARY KEY
-    (id)
-);
 
-    CREATE TABLE activities
-    (
-        id INTEGER
-        AUTO_INCREMENT NOT NULL,
-    name VARCHAR
-        (255) NOT NULL,
-    createdAt DATETIME,
-    PRIMARY KEY
-        (id) 
-)
