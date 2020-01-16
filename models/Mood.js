b00d4b1fe67bca4e50a6b74ea01b1e16ae59f254
David Vega <dvega920@gmail.com>
2020-01-15 22:06:44 -0500

    merge conflict resolved

--- models/Mood.js
+++ models/Mood.js
@@ -1,9 +1,4 @@
 module.exports = (sequelize, DataTypes) => {
-<<<<<<< HEAD
-  const Mood = sequelize.define('mood', {
-    mood_name: DataTypes.STRING,
-    rating: DataTypes.INTEGER
-=======
   const Mood = sequelize.define('Moods', {
     emotion: {
       type: DataTypes.STRING,
@@ -13,7 +8,6 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       allowNull: false,
     }
->>>>>>> 6b49ba4349a6e735012e453334d17d18d969d477
   });
   return Mood;
 };
