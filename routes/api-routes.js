c287e61672233f218037ab801987f36db33f391b
David Vega <dvega920@gmail.com>
2020-01-16 10:12:10 -0500

    reverted back to Kyle's template

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,12 +1,12 @@
-// const router = require('express').Router();
-const db = require('../models');
+const router = require('express').Router();
+// const db = require('../models');
 
-module.exports = function (router) {
-    router.get('/api', (req, res) => {
-        db.Mood.findAll({})
-            .then((dbMoods) => {
-                res.json(dbMoods);
-                // console.log(dbMoods);
-            });
-    });
-};
+router.get('/api', (req, res) => {
+    db.Mood.findAll({})
+        .then((dbMoods) => {
+            res.json(dbMoods);
+            // console.log(dbMoods);
+        });
+});
+
+module.exports = router;
