656597eac9a24aa7be8e2bc0dece7452d5f9da7d
David Vega <dvega920@gmail.com>
2020-01-17 18:36:56 -0500

    added 's' in table definition

--- models/Mood.js
+++ models/Mood.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
-  const Mood = sequelize.define('Mood', {
+  const Mood = sequelize.define('Moods', {
     emotion: DataTypes.STRING,
     rating: DataTypes.INTEGER
   });
