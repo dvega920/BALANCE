ca0afb29b64ac597484efb1cb054ebaef7abc299
David Vega <dvega920@gmail.com>
2020-01-17 19:43:50 -0500

    resloved merge conflict with jamie-2

--- models/Mood.js
+++ models/Mood.js
@@ -1,11 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
-<<<<<<< HEAD
-  const Mood = sequelize.define('Moods', {
-    emotion: DataTypes.STRING,
-=======
   const Mood = sequelize.define('Mood', {
     mood_name: DataTypes.STRING,
->>>>>>> e4ed8be94bb75e4759c1db1a2c4077ad717b2230
     rating: DataTypes.INTEGER
   });
   return Mood;
