945032d6d30b5fa25ac9b495105a0bf18cc696f2
David Vega <dvega920@gmail.com>
2020-03-03 21:32:48 -0500

    added vuetify and vue-router

--- models/Mood.js
+++ models/Mood.js
@@ -1,17 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
-<<<<<<< HEAD
-<<<<<<< HEAD
   const Mood = sequelize.define('Moods', {
     emotion: DataTypes.STRING,
-=======
-  const Mood = sequelize.define('Mood', {
-    mood_name: DataTypes.STRING,
->>>>>>> 91407186939f37dc4c69b1a6c9a574e9deca215b
-=======
-  const Mood = sequelize.define('Mood', {
-    mood_name: DataTypes.STRING,
->>>>>>> ca0afb29b64ac597484efb1cb054ebaef7abc299
-    rating: DataTypes.INTEGER
-  });
+  })
   return Mood;
 };
