cc4db3443faf3748804d1e242842919a0ddd9326
David Vega <dvega920@gmail.com>
2020-03-06 11:23:46 -0500

    corrected new instance of router  from new Vue to new VueRouter

--- client/src/main.js
+++ client/src/main.js
@@ -1,24 +1,22 @@
 import Vue from 'vue';
 import App from './App.vue';
-import vuetify from './plugins/vuetify';
-import VueRouter from 'vue-router'
-import HelloWorld from './components/HelloWorld'
+import VueRouter from 'vue-router';
+import HelloWorld from './components/HelloWorld';
 import Patient from './components/Patient'
 
 Vue.use(VueRouter);
 
 Vue.config.productionTip = false
 
-const router = new Vue({
+const router = new VueRouter({
   routes: [
     { path: '/', component: HelloWorld },
-    { path: '/Patient', component: Patient },
+    { path: '/Patient', component: Patient }
   ],
   mode: 'history'
 })
 
 new Vue({
   router,
-  vuetify,
   render: h => h(App)
 }).$mount('#app')
