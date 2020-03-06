cf56f0350a57dae0d9b32870ca3468ec3be33873
David Vega <dvega920@gmail.com>
2020-03-05 20:56:39 -0500

    added vue-router to front end

---
+++ client/src/components/PatientLogin.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>This is the Patient Log In</h1>
+</template>
+
+<script>
+export default {
+  name: "PatientLogin"
+};
+</script>
+
+<style>
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import vuetify from './plugins/vuetify';
+import VueRouter from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false
 
+const router = new Vue({
+  routes: [
+    { path: '/', component: HelloWorld },
+  ],
+  mode: 'history'
+})
+
 new Vue({
+  router,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
