47bf705bd6d2d1af2e1f95a58436ca7a91e119e9
David Vega <dvega920@gmail.com>
2020-03-06 11:19:25 -0500

    added router-view

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,8 @@
 <template>
-  <div id="App">
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld />
-    <!-- <router-view></router-view> -->
+    <router-view></router-view>
   </div>
 </template>
 
@@ -12,10 +13,17 @@ export default {
   name: "App",
   components: {
     HelloWorld
-  },
-
-  data: () => ({
-    //
-  })
+  }
 };
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
