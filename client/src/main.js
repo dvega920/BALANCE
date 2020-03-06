d173f337def8ffe9306d1374e25c368d95ad7b97
David Vega <dvega920@gmail.com>
2020-03-06 11:52:32 -0500

    imported veutify

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import App from './App.vue';
 import VueRouter from 'vue-router';
+import Vuetify from 'vuetify';
 import HelloWorld from './components/HelloWorld';
 import Patient from './components/Patient'
 
 Vue.use(VueRouter);
+Vue.use(Vuetify);
 
 Vue.config.productionTip = false
 
