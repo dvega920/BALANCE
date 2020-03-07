523087be740df248698686ec7139453499f82a35
David Vega <dvega920@gmail.com>
2020-03-07 13:26:52 -0500

    added Dashboard component made dashboard default route

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 import VueRouter from 'vue-router';
-import Home from './components/Home';
+// import Home from './components/Home';
 import PatientLogin from './components/PatientLogin';
 import ProviderLogin from './components/ProviderLogin';
+import Dashboard from './components/Dashboard';
 import vuetify from './plugins/vuetify';
 
 Vue.use(VueRouter);
@@ -11,17 +12,21 @@ Vue.use(VueRouter);
 
 Vue.config.productionTip = false
 
-const router = new VueRouter({
-  routes: [
-    { path: '/', component: Home },
-    { path: '/Patient-Login', component: PatientLogin },
-    { path: '/Provider-Login', component: ProviderLogin },
-  ],
-  mode: 'history'
-})
+
+  const router = new VueRouter({
+    routes: [
+      // { path: '/', name: 'Home', component: Home },
+      { path: '/', name: 'PatientLogin', component: PatientLogin },
+      { path: '/Provider-Login', name: 'ProviderLogin', component: ProviderLogin },
+      { path: '/Dashboard', name:'Dashboard', component: Dashboard },
+    ],
+    mode: 'history'
+  })
 
 new Vue({
   router,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
+
+export default router;
