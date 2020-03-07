d0f1d4387289974ebca43485eb5342b91f5e0bc9
David Vega <dvega920@gmail.com>
2020-03-07 13:27:43 -0500

    created Dashboard component

---
+++ client/src/components/Dashboard.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h2>Dashboard</h2>
+        <p>Name: {{ user.name }}</p>
+    </div>
+</template>
+
+<script>
+import router from "../main.js"
+import axios from "axios"
+
+export default {
+  name: "Login",
+  data () {
+    return {
+      user: {
+        name: "Jesse"
+      }
+    }
+  },
+  methods: {
+    getUserData: function () {
+      let self = this
+      axios.get("/api/user")
+        .then((response) => {
+          console.log(response)
+          self.$set(this, "user", response.data.user)
+        })
+        .catch((errors) => {
+          console.log(errors)
+          router.push("/")
+        })
+    }
+  },
+  mounted () {
+    this.getUserData()
+  }
+}
+</script>
