ff4447b175cbb8c3d0e802382198f640a8d0cfba
David Vega <dvega920@gmail.com>
2020-03-08 14:18:06 -0400

    uncommented our Vuetify import

--- client/src/main.js
+++ client/src/main.js
@@ -7,7 +7,7 @@ import ProviderLogin from './components/ProviderLogin';
 // import Dashboard from './components/Dashboard';
 import Questionnaire from './components/Questionnaire'
 import vuetify from './plugins/vuetify';
-// import Vuetify from 'vuetify/lib';
+import Vuetify from 'vuetify/lib';
 import { BootstrapVue } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
@@ -15,7 +15,7 @@ import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.use(VueRouter);
 Vue.use(BootstrapVue);
-// Vue.use(Vuetify);
+Vue.use(Vuetify);
 
 Vue.config.productionTip = false
 
@@ -34,6 +34,7 @@ Vue.config.productionTip = false
 new Vue({
   router,
   vuetify,
+  Vuetify,
   render: h => h(App)
 }).$mount('#app')
 
