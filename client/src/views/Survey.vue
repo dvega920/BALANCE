3fecf17af8e05e0e7bbd18650005ef61efb4c5a5
David Vega <dvega920@gmail.com>
2020-03-10 19:52:52 -0400

    added Questionnaire component

--- client/src/views/Survey.vue
+++ client/src/views/Survey.vue
@@ -2,25 +2,25 @@
   <v-container>
     <v-layout>
       <v-flex justify-center>
-         <v-container fluid>
-       <mySurvey />
-    
-  
-  </v-container> 
-    
+        <v-container fluid>
+          <mySurvey />
+          <Questionnaire />
+        </v-container>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+import Questionnaire from "../components/Questionnaire.vue";
 import mySurvey from "../components/mySurvey";
 // @ is an alias to /src
 
 export default {
   name: "App",
   components: {
-    mySurvey
-  },
-}
+    mySurvey,
+    Questionnaire
+  }
+};
 </script>
