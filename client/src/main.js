1d25b6f4eddf5fdb2550b1b525535f5bb3ad8bcc
David Vega <dvega920@gmail.com>
2020-03-09 21:23:28 -0400

    commented out Veutify

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
-import Vuetify from 'vuetify/lib';
+// import Vuetify from 'vuetify/lib';
 // import { BootstrapVue } from 'bootstrap-vue';
 // import 'bootstrap-vue/dist/bootstrap-vue.css';
 // import 'bootstrap/dist/css/bootstrap.css';
