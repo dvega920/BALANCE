c2251f7ee6b52c50fa7672a8bf6b4980a227cef4
David Vega <dvega920@gmail.com>
2020-03-10 21:09:54 -0400

    added precode for heroku deploy

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const db = require('./models');
 const routes = require('./routes');
+const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'));
+// Serve static files from the Vue app build directory
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
 app.use('/', routes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
+});
+
 // Sync sequelize models then start Express app
 // =============================================
-db.sequelize.sync().then(() => {
+db.sequelize.sync(/*{ force: true }*/).then(() => {
   app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`);
   });
