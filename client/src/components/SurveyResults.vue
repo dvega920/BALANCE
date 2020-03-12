42005f464b4a8843fbebe56f0b71572d5349fa25
Amanda Foster <mandolinfoster@gmail.com>
2020-03-11 23:44:28 -0400

    Got values and questions to populate. Committing before I break it again.

--- client/src/components/SurveyResults.vue
+++ client/src/components/SurveyResults.vue
@@ -4,7 +4,12 @@
       <v-flex justify-center>
         <v-container fluid>
           <!-- <SurveyResults /> -->
-          <v-data-table :headers="headers" :questions="name" class="elevation-1"></v-data-table>
+          <v-data-table
+            :headers="headers"
+            :items="questions"
+            :questions-per-page="5"
+            class="elevation-1"
+          ></v-data-table>
         </v-container>
       </v-flex>
     </v-layout>
@@ -22,43 +27,67 @@ export default {
           sortable: false,
           value: "name"
         },
-        { text: "No Risk", value: "0-8" },
-        { text: "Mild Risk", value: "8-16" },
-        { text: "Moderate Risk", value: "16-24" },
-        { text: "Severe Risk", value: "24-32" }
+        { text: "No Risk (%)", value: "norisk" },
+        { text: "Mild Risk (%)", value: "mildrisk" },
+        { text: "Moderate Risk (%)", value: "modrisk" },
+        { text: "Severe Risk (%)", value: "sevrisk" }
       ],
       questions: [
         {
           name: "Interest in Life",
-          value: "8"
+          norisk: 0,
+          mildrisk: 80,
+          modrisk: 10,
+          sevrisk: 10
         },
         {
           name: "Down or Depressed",
-          value: "16"
+          norisk: 5,
+          mildrisk: 15,
+          modrisk: 50,
+          sevrisk: 30
         },
         {
           name: "Trouble with Sleep",
-          value: "2"
+          norisk: 40,
+          mildrisk: 22,
+          modrisk: 18,
+          sevrisk: 20
         },
         {
           name: "Tired or Little Energy",
-          value: "16"
+          norisk: 0,
+          mildrisk: 85,
+          modrisk: 10,
+          sevrisk: 5
         },
         {
           name: "Poor Appetite or Overeating",
-          value: "24"
+          norisk: 0,
+          mildrisk: 0,
+          modrisk: 85,
+          sevrisk: 15
         },
         {
           name: "Feeling Bad About Yourself",
-          value: "2"
+          norisk: 0,
+          mildrisk: 25,
+          modrisk: 57,
+          sevrisk: 18
         },
         {
           name: "Moving or Speaking slowly",
-          value: "5"
+          norisk: 90,
+          mildrisk: 0,
+          modrisk: 10,
+          sevrisk: 0
         },
         {
           name: "Suicidal Thoughts",
-          value: "11"
+          norisk: 0,
+          mildrisk: 0,
+          modrisk: 86,
+          sevrisk: 14
         }
       ]
     };
